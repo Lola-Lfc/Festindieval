4414658861907403066cc6b86e504e63afb2f938
Loïc.Lfc <loic.lefranc83@gmail.com>
2026-09-23 17:27:37 +0200

    Add Invite Test

--- SiteWeb/resources/views/invite.blade.php
+++ SiteWeb/resources/views/invite.blade.php
@@ -2,6 +2,25 @@
 
 @foreach ($invites as $invite)
     <h2>{{ $invite->nom }}</h2>
+
+    <img src="{{ asset($invite->pfp) }}" alt="{{ $invite->nom }}">
+
     <p>{{ $invite->description }}</p>
     <p>{{ $invite->activite }}</p>
+
+    @if ($invite->youtube)
+        <a href="{{ $invite->youtube }}" target="_blank">YouTube</a>
+    @endif
+
+    @if ($invite->instagram)
+        <a href="{{ $invite->instagram }}" target="_blank">Instagram</a>
+    @endif
+
+    @if ($invite->tiktok)
+        <a href="{{ $invite->tiktok }}" target="_blank">TikTok</a>
+    @endif
+
+    @if ($invite->site_web)
+        <a href="{{ $invite->site_web }}" target="_blank">Site web</a>
+    @endif
 @endforeach
