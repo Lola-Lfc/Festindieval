b4b4aae85e5d896cbc4687157da8005dfcddc0a9
Loïc.Lfc <loic.lefranc83@gmail.com>
2026-09-23 12:19:52 +0200

    Init Database

--- SiteWeb/database/migrations/2026_09_23_094829_create_typebillet_table.php
+++ SiteWeb/database/migrations/2026_09_23_094829_create_typebillet_table.php
@@ -6,22 +6,19 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
-        Schema::create('typebillet', function (Blueprint $table) {
+        Schema::create('typebillets', function (Blueprint $table) {
             $table->id();
+            $table->string('nom');
+            $table->decimal('prix', 10, 2);
+            $table->text('description')->nullable();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
-        Schema::dropIfExists('typebillet');
+        Schema::dropIfExists('typebillets');
     }
 };
