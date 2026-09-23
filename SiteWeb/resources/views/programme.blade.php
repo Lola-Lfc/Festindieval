ca6a240f6eefae6ba69a1d67ba1559915801d1ab
lisabruno <lisa.bruno@mmibordeaux.com>
2026-09-23 14:37:56 +0200

    test views blades fonctionnels

---
+++ SiteWeb/resources/views/programme.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Programme - Festindival</title>
+</head>
+<body>
+    <h1>Programme de Festindival</h1>
+
+    <p>Tournoi medieval a 14h.</p>
+    <p>Banquet medieval a 19h.</p>
+
+    <a href="/">Retour a l'accueil</a>
+</body>
+</html>
