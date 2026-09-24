abae5e3242c3f0c73b63ab9515f6769b33423795
Loïc.Lfc <loic.lefranc83@gmail.com>
2026-09-24 10:11:05 +0200

    Basic front for billeterie

--- SiteWeb/resources/views/billeterie.blade.php
+++ SiteWeb/resources/views/billeterie.blade.php
@@ -1,7 +1,37 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Billetterie</title>
+    <link rel="stylesheet" href="{{ asset('css/billeterie.css') }}">
+</head>
+<body>
+
 <h1>Billetterie</h1>
 
-@foreach ($typebillets as $billet)
-    <h2>{{ $billet->nom }}</h2>
-    <p>{{ $billet->description }}</p>
-    <p>{{ $billet->prix }} €</p>
-@endforeach
+<div class="billets-container">
+
+    @foreach ($typebillets as $billet)
+
+        <div class="billet-card">
+
+            <h2>{{ $billet->nom }}</h2>
+
+            <p class="prix">
+                {{ number_format($billet->prix, 2, ',', ' ') }} €
+            </p>
+
+            <p class="description">
+                {{ $billet->description }}
+            </p>
+
+            <button>Acheter</button>
+
+        </div>
+
+    @endforeach
+
+</div>
+
+</body>
+</html>
