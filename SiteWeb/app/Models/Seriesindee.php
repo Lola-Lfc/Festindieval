889f78aa2b0c3f8a792aa571238cc21cd0d81364
Loïc.Lfc <loic.lefranc83@gmail.com>
2026-09-24 09:15:40 +0200

    Basic Front For Series

--- SiteWeb/app/Models/Seriesindee.php
+++ SiteWeb/app/Models/Seriesindee.php
@@ -3,8 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Seriesindee extends Model
 {
     protected $guarded = [];
+
+    public function dons(): HasMany
+    {
+        return $this->hasMany(Don::class, 'serie_id');
+    }
 }
--- SiteWeb/resources/views/serie.blade.php
+++ SiteWeb/resources/views/serie.blade.php
@@ -1,9 +1,37 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Séries</title>
+    <link rel="stylesheet" href="{{ asset('css/series.css') }}">
+</head>
+<body>
+
 <h1>Séries</h1>
 
-@foreach ($series as $serie)
-    <h2>{{ $serie->nom }}</h2>
-    <img src="{{ asset($serie->image) }}" alt="{{ $serie->nom }}">
-    <p>{{ $serie->description }}</p>
-    <a href="{{ $serie->lien }}" target="_blank">Regarder la série</a>
-    <br>
-@endforeach
+<div class="series-container">
+    @foreach ($series as $serie)
+        <div class="serie-card">
+
+            <img src="{{ asset($serie->image) }}" alt="{{ $serie->nom }}">
+
+            <div class="serie-content">
+                <h2>{{ $serie->nom }}</h2>
+
+                <p>{{ $serie->description }}</p>
+
+                <p class="cagnotte">
+                    Cagnotte récoltée : {{ $serie->dons->sum('montant') }} €
+                </p>
+
+                <a href="{{ $serie->lien }}" target="_blank">
+                    Regarder la série
+                </a>
+            </div>
+
+        </div>
+    @endforeach
+</div>
+
+</body>
+</html>
